9d35b7fb9cfa3ee846297f68a3fe8de716ee10d3
RickyBoy <rmsmp2003@gmail.com>
2025-09-27 20:02:03 +0100

    added some functions

--- src/funcs/feature_engineering.py
+++ src/funcs/feature_engineering.py
@@ -1,4 +1,6 @@
 from typing import Literal
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
 
 import pandas as pd
 
@@ -57,3 +59,74 @@ class Preprocessing:
             data.drop(toRemove, axis=0, inplace=True)
 
         return data
+    
+    @staticmethod
+    def encodeCategorial(
+        data: pd.DataFrame,
+        catFeatures: list[str]
+        
+        
+    ) -> pd.DataFrame:
+            
+        """Encode categorical variables with LabelEncoder
+        
+        
+            Args:
+                data (`pd.DataFrame`): Dataframe to be treated
+                catFeatures: list[str]: list of categorical column names that need encoding
+        
+            Returns:
+                `pd.DataFrame`: Categorical columns are now numeric labels
+        """
+        le = LabelEncoder()
+        for var in catFeatures:
+            data[var] =  le.fit_transform(data[var].astype(str))
+        return data
+    
+    
+    @staticmethod
+    def scaleFeatures(
+        data: pd.DataFrame, 
+        metricFeatures: list[str]
+    ) -> pd.DataFrame:
+        
+        
+        """Standardize numeric features
+        
+        Args:
+            data (`pd.DataFrame`): Dataframe to be treated
+            metricFeatures: list[str]: list of numeric column names
+            
+        Returns:
+            `pd.DataFrame`:DataFrame where numeric columns are scaled.
+        
+
+        
+        """
+        scaler = StandardScaler()
+        data[metricFeatures] = scaler.fit_transform(data[metricFeatures])
+        return data
+    
+    
+    @staticmethod
+    def splitdata(
+        data: pd.DataFrame,
+        target: str,
+        test_size: float = 0.2, 
+        random_state: int = 42, 
+        stratify: bool = True,
+        
+    ):
+        x = data.drop(columns=[target]) 
+        y = data[target]
+        return train_test_split( 
+            x, 
+            y, 
+            test_size=test_size, 
+            random_state=random_state, 
+            stratify=y 
+            if stratify else None, 
+            
+        )
+        
+        
