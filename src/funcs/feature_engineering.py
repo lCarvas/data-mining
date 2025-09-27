2d2ea3a881792b5db2a73d42f750bb22c82178e1
Carvas <76731842+lCarvas@users.noreply.github.com>
2025-09-27 21:56:58 +0100

    chore: fixed linting and formatting mistakes

--- src/funcs/feature_engineering.py
+++ src/funcs/feature_engineering.py
@@ -1,8 +1,8 @@
 from typing import Literal
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.model_selection import train_test_split
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 
 class Preprocessing:
@@ -59,74 +59,60 @@ class Preprocessing:
             data.drop(toRemove, axis=0, inplace=True)
 
         return data
-    
+
     @staticmethod
     def encodeCategorial(
-        data: pd.DataFrame,
-        catFeatures: list[str]
-        
-        
+        data: pd.DataFrame, catFeatures: list[str]
     ) -> pd.DataFrame:
-            
         """Encode categorical variables with LabelEncoder
-        
-        
-            Args:
-                data (`pd.DataFrame`): Dataframe to be treated
-                catFeatures: list[str]: list of categorical column names that need encoding
-        
-            Returns:
-                `pd.DataFrame`: Categorical columns are now numeric labels
+
+
+        Args:
+            data (`pd.DataFrame`): Dataframe to be treated
+            catFeatures: list[str]: list of categorical column names that need encoding
+
+        Returns:
+            `pd.DataFrame`: Categorical columns are now numeric labels
         """
         le = LabelEncoder()
         for var in catFeatures:
-            data[var] =  le.fit_transform(data[var].astype(str))
+            data[var] = le.fit_transform(data[var].astype(str))
         return data
-    
-    
+
     @staticmethod
     def scaleFeatures(
-        data: pd.DataFrame, 
-        metricFeatures: list[str]
+        data: pd.DataFrame, metricFeatures: list[str]
     ) -> pd.DataFrame:
-        
-        
         """Standardize numeric features
-        
+
         Args:
             data (`pd.DataFrame`): Dataframe to be treated
             metricFeatures: list[str]: list of numeric column names
-            
+
         Returns:
             `pd.DataFrame`:DataFrame where numeric columns are scaled.
-        
 
-        
+
+
         """
         scaler = StandardScaler()
         data[metricFeatures] = scaler.fit_transform(data[metricFeatures])
         return data
-    
-    
+
     @staticmethod
     def splitdata(
         data: pd.DataFrame,
         target: str,
-        test_size: float = 0.2, 
-        random_state: int = 42, 
+        test_size: float = 0.2,
+        random_state: int = 42,
         stratify: bool = True,
-        
     ):
-        x = data.drop(columns=[target]) 
+        x = data.drop(columns=[target])
         y = data[target]
-        return train_test_split( 
-            x, 
-            y, 
-            test_size=test_size, 
-            random_state=random_state, 
-            stratify=y 
-            if stratify else None, 
-            
+        return train_test_split(
+            x,
+            y,
+            test_size=test_size,
+            random_state=random_state,
+            stratify=y if stratify else None,
         )
-        
-        
